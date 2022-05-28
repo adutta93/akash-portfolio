43bf2f8e38e64e6d537542193eb0215d2312dafb
akash.dutta <dutta.akash1993official@gmail.com>
2022-05-28 22:20:14 +0530

    portfolio migrated to www.akashdutta.tech

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React from 'react';
-import './App.css';
-
-import Header from './components/Header';
-import About from './components/About';
-import Work from './components/work';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
-import Experience from './components/experience';
-import Tech from './components/tech';
-import Blog from './components/blog';
 
 function App() {
 	return (
-		<div className='App'>
-			<Navbar />
-			<div className='App-header'>
-				<Header />
-			</div>
-
-			<div className='About'>
-				<About />
-			</div>
-			<div className='tech'>
-				<Tech />
-			</div>
-			<div className='experience'>
-				<Experience />
-			</div>
-			<div className='work'>
-				<Work />
-			</div>
-			<div className='blog'>
-				<Blog />
-			</div>
-			<div className='contact'>
-				<Contact />
-			</div>
+		<div className='App' style={{ marginTop: '5rem' }}>
+			Website migrated to <a href='https://akashdutta.tech/'> www.akashdutta.tech</a>
 		</div>
 	);
 }
--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ value }) => {
-  return <button className="all-btn">{value}</button>;
-};
-
-export default Button;
